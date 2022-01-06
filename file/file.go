03036d0816f7f6e202d6c4f8b1e563b9231c2f6a
yasin <yascanary0@gmail.com>
2022-01-06 16:20:55 +0330

    create file module

---
+++ file/file.go
@@ -0,0 +1,39 @@
+package file
+
+import (
+	"bufio"
+	"os"
+)
+
+func NewFile(path string, value string) {
+	file, err := os.Create(path)
+
+	// TODO: use err handler here
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	file.WriteString(value)
+
+	file.Sync()
+}
+
+func AddToFile(path string, value string) {
+	file, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	// TODO: use err handler here
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+
+	writer.WriteString("\n")
+	writer.WriteString(value)
+
+	writer.Flush()
+}
