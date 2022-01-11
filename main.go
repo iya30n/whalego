4db69f9ed365ee11ff445d53de7a284cc1c5c507
iya30n <varmeziyaryasin@gmail.com>
2022-01-11 13:40:27 +0330

    make commands for application

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"whalego/database/migration"
 	"whalego/services/telegram/ProxyService"
 )
 
 func main() {
-	migration.Migrate()
+	if len(os.Args) < 2 {
+		fmt.Println("what can i do for you?")
+		os.Exit(1)
+	}
 
-	ProxyService.New().GetProxies()
+	if os.Args[1] == "migrate" {
+		migration.Migrate()
+		os.Exit(1)
+	}
+
+	if os.Args[1] == "proxy:crawler" {
+		migration.Migrate()
+		ProxyService.New().GetProxies()
+	}
 }
