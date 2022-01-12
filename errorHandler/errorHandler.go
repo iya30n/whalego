ecc13df55a2982630d25b273c9b4d1cfab577c4f
iya30n <varmeziyaryasin@gmail.com>
2022-01-12 12:44:55 +0330

    save file:line of error

--- errorHandler/errorHandler.go
+++ errorHandler/errorHandler.go
@@ -1,13 +1,22 @@
 package errorHandler
 
 import (
+	// "fmt"
+	"fmt"
 	"os"
+	"runtime"
 	"whalego/file"
 )
 
 func LogFile(err error) {
 	if err != nil {
-		file.AddOrCreate("./errors.txt", err.Error())
+		_, errfile, line, _ := runtime.Caller(1)
+
+		errorMessage := err.Error() + " (file: " + errfile + ":%d)"
+
+		errorMessage = fmt.Sprintf(errorMessage, line)
+
+		file.AddOrCreate("./errors.txt", errorMessage)
 
 		os.Exit(1)
 	}
