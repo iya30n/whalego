4f1ad916c30f7b3518878da9513584e0b17e4be9
yasin <yascanary0@gmail.com>
2022-01-13 20:27:57 +0330

    check all message types for proxy

--- services/telegram/ProxyService/ProxyService.go
+++ services/telegram/ProxyService/ProxyService.go
@@ -103,9 +103,25 @@ func (ps *ProxyService) SendProxy() {
 * get message url from content key
  */
 func (ps *ProxyService) textMessageHandler(message *client.Message) string {
-	content := message.Content.(*client.MessageText).Text.Entities[0]
+	contentType := message.Content.MessageContentType()
+	if contentType != client.TypeMessageText && contentType != client.TypeMessage {
+		return ""
+	}
 
-	url := content.Type.(*client.TextEntityTypeTextUrl).Url
+	entities := message.Content.(*client.MessageText).Text.Entities
+
+	var url string
+	for _, entity := range entities {
+		if entity.Type.TextEntityTypeType() != client.TypeTextEntityTypeTextUrl {
+			continue
+		}
+
+		url = entity.Type.(*client.TextEntityTypeTextUrl).Url
+
+		if url != "" {
+			break
+		}
+	}
 
 	return url
 }
