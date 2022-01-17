95a45bc5006cb100c6f6b719da4f136680b5ed52
iya30n <varmeziyaryasin@gmail.com>
2022-01-17 14:04:52 +0330

    fix button nil err

--- services/telegram/ProxyService/ProxyService.go
+++ services/telegram/ProxyService/ProxyService.go
@@ -133,7 +133,16 @@ func (ps *ProxyService) textMessageHandler(message *client.Message) string {
 * get message url from message button key
  */
 func (ps *ProxyService) buttonMessageHandler(message *client.Message) string {
+
+	if message.ReplyMarkup == nil {
+		return ""
+	}
+
 	replyMarkup := message.ReplyMarkup.(*client.ReplyMarkupInlineKeyboard).Rows[0][0]
+
+	if replyMarkup.Type.InlineKeyboardButtonTypeType() != client.TypeInlineKeyboardButtonTypeUrl {
+		return ""
+	}
 
 	url := replyMarkup.Type.(*client.InlineKeyboardButtonTypeUrl).Url
 
