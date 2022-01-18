be0a8f2407c2373f579e2c604f07152eca86a799
iya30n <varmeziyaryasin@gmail.com>
2022-01-18 12:03:23 +0330

    Revert "use close chat to try fixing chat not found error"
    This reverts commit 92f14f633870a1017c938e2dd48ef190835fca98.

--- services/telegram/MessageService/MessageService.go
+++ services/telegram/MessageService/MessageService.go
@@ -19,12 +19,8 @@ func New() *MessageService {
 }
 
 func (ms *MessageService) GetMessages(chatId int64, fromMessage int64) *client.Messages {
-	defer ms.tgConnection.CloseChat(&client.CloseChatRequest{
-		ChatId: chatId,
-	})
-
 	defer connection.Close(ms.tgConnection)
-	// ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
+	ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
 	result, err := ms.tgConnection.GetChatHistory(&client.GetChatHistoryRequest{
 		ChatId: chatId,
 		// FromMessageId: fromMessage,
@@ -39,12 +35,8 @@ func (ms *MessageService) GetMessages(chatId int64, fromMessage int64) *client.M
 }
 
 func (ms *MessageService) SendMessage(chatId int64, message client.InputMessageContent) *client.Message {
-	defer ms.tgConnection.CloseChat(&client.CloseChatRequest{
-		ChatId: chatId,
-	})
-
 	defer connection.Close(ms.tgConnection)
-	// ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
+	ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
 	msg, err := ms.tgConnection.SendMessage(&client.SendMessageRequest{
 		ChatId:              chatId,
 		InputMessageContent: message,
@@ -56,10 +48,6 @@ func (ms *MessageService) SendMessage(chatId int64, message client.InputMessageC
 }
 
 func (ms *MessageService) SendMarkdown(chatId int64, message string) *client.Message {
-	defer ms.tgConnection.CloseChat(&client.CloseChatRequest{
-		ChatId: chatId,
-	})
-
 	defer connection.Close(ms.tgConnection)
 	mdMsg, err := ms.tgConnection.ParseMarkdown(&client.ParseMarkdownRequest{
 		Text: &client.FormattedText{
@@ -77,7 +65,7 @@ func (ms *MessageService) SendMarkdown(chatId int64, message string) *client.Mes
 
 	errorHandler.LogFile(err)
 
-	// ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
+	ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
 	msg, err := ms.tgConnection.SendMessage(&client.SendMessageRequest{
 		ChatId: chatId,
 		InputMessageContent: &client.InputMessageText{
@@ -91,10 +79,6 @@ func (ms *MessageService) SendMarkdown(chatId int64, message string) *client.Mes
 }
 
 func (ms *MessageService) DeleteMessages(chatId int64, messageIds []int64) {
-	defer ms.tgConnection.CloseChat(&client.CloseChatRequest{
-		ChatId: chatId,
-	})
-
 	defer connection.Close(ms.tgConnection)
 	ms.tgConnection.LoadChats(&client.LoadChatsRequest{})
 	if len(messageIds) < 1 {
