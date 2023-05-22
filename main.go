e96e03af15273cacd0ee0e6dcdb2672f3fbd2deb
yasin <yascanary0@gmail.com>
2023-05-22 16:14:00 +0330

    keep connect the app after running each command

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"log"
+	// "os"
+	"whalego/connection"
 	"whalego/database/migration"
 	"whalego/errorHandler"
 	"whalego/models/Channel"
@@ -12,38 +14,65 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("what can i do for you?")
-		os.Exit(1)
-	}
+	for {
+		var command string
 
-	if os.Args[1] == "migrate" {
-		migration.Migrate()
-		os.Exit(1)
-	}
+		fmt.Scanln(&command)
 
-	if os.Args[1] == "proxy:crawler" {
-		channels := Channel.New()
-		for _, channel := range channels.All() {
-			ProxyService.GetProxies(&channel)
+		if len(command) == 0 {
+			break
 		}
 
-		os.Exit(1)
+		if command == "migrate" {
+			fmt.Println("command migrate is running...")
+			migration.Migrate()
+			continue
+		}
+
+		if command == "proxy:crawler" {
+			fmt.Println("command proxy:crawler is running...")
+			channels := Channel.New()
+			for _, channel := range channels.All() {
+				ProxyService.GetProxies(&channel)
+			}
+
+			continue
+		}
+
+		if command == "proxy:send" {
+			fmt.Println("command proxy:send is running...")
+			ProxyService.SendProxy()
+			continue
+		}
+
+		if command == "proxy:check" {
+			fmt.Println("command proxy:check is running...")
+			ProxyService.CheckAllProxies()
+			continue
+		}
+
+		if command == "test" {
+			fmt.Println("command test is running...")
+			chatID, err := ChatService.GetChatId("iya30n")
+			errorHandler.LogFile(err)
+			MessageService.SendMarkdown(chatID.Id, "salam from whale")
+
+			continue
+		}
+
+		if command == "getme" {
+			fmt.Println("command getme is running...")
+			con := connection.TdConnection(true)
+			me, err := con.GetMe()
+			errorHandler.LogFile(err)
+			log.Printf("Me: %s %s [%s]", me.FirstName, me.LastName, me.Username)
+			continue
+		}
 	}
 
-	if os.Args[1] == "proxy:send" {
-		ProxyService.SendProxy()
+	/* if len(os.Args) < 2 {
+		fmt.Println("what can i do for you?")
 		os.Exit(1)
-	}
+	} */
 
-	if os.Args[1] == "proxy:check" {
-		ProxyService.CheckAllProxies()
-		os.Exit(1)
-	}
-
-	if os.Args[1] == "test" {
-		chatID, err := ChatService.GetChatId("iya30n")
-		errorHandler.LogFile(err)
-		MessageService.SendMarkdown(chatID.Id, "salam from whale")
-	}
 }
