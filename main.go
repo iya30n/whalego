4f148277128a09f81b99478631d6dbe548eea0c7
yasin <yascanary0@gmail.com>
2023-05-26 17:54:09 +0330

    use ticker to schedule commands

--- main.go
+++ main.go
@@ -3,69 +3,69 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 	"whalego/connection"
 	"whalego/database/migration"
 	"whalego/errorHandler"
 	"whalego/models/Channel"
-	"whalego/services/telegram/ChatService"
-	"whalego/services/telegram/MessageService"
+	// "whalego/services/telegram/ChatService"
+	// "whalego/services/telegram/MessageService"
 	"whalego/services/telegram/ProxyService"
 )
 
 func main() {
-	for {
-		var command string
 
-		fmt.Scanln(&command)
+	if len(os.Args) > 1 {
+		fmt.Println("command migrate is running...")
 
-		if len(command) == 0 {
-			break
-		}
-
-		if command == "migrate" {
-			fmt.Println("command migrate is running...")
+		switch os.Args[1] {
+		case "migrate":
 			migration.Migrate()
-			continue
-		}
-
-		if command == "proxy:crawler" {
-			fmt.Println("command proxy:crawler is running...")
-			channels := Channel.New()
-			for _, channel := range channels.All() {
-				ProxyService.GetProxies(&channel)
-			}
-
-			continue
-		}
-
-		if command == "proxy:send" {
-			fmt.Println("command proxy:send is running...")
-			ProxyService.SendProxy()
-			continue
-		}
-
-		if command == "proxy:check" {
-			fmt.Println("command proxy:check is running...")
-			ProxyService.CheckAllProxies()
-			continue
-		}
-
-		if command == "test" {
-			fmt.Println("command test is running...")
-			chatID, err := ChatService.GetChatId("iya30n")
-			errorHandler.LogFile(err)
-			MessageService.SendMarkdown(chatID.Id, "salam from whale")
-
-			continue
-		}
-
-		if command == "getme" {
+		case "test":
 			fmt.Println("command getme is running...")
 			con := connection.TdConnection(true)
 			me, err := con.GetMe()
 			errorHandler.LogFile(err)
 			log.Printf("Me: %s %s [%s]", me.FirstName, me.LastName, me.Username)
-			continue
+		default:
+			log.Println("Invalid argument!")
+		}
+
+		return
+	}
+
+	crawlerTicker := time.NewTicker(time.Hour * 2)
+
+	sendTicker := time.NewTicker(time.Hour)
+
+	checkTicker := time.NewTicker(time.Hour * 6)
+
+	for {
+		select {
+		case <-crawlerTicker.C:
+			channels := Channel.New()
+			for _, channel := range channels.All() {
+				ProxyService.GetProxies(&channel)
+			}
+
+		case <-sendTicker.C:
+			ProxyService.SendProxy()
+
+		case <-checkTicker.C:
+			ProxyService.CheckAllProxies()
 		}
 	}
+
+	/* fmt.Println("command test is running...")
+	chatID, err := ChatService.GetChatId("iya30n")
+	errorHandler.LogFile(err)
+	MessageService.SendMarkdown(chatID.Id, "salam from whale")
+
+	fmt.Println("command getme is running...")
+	con := connection.TdConnection(true)
+	me, err := con.GetMe()
+	errorHandler.LogFile(err)
+	log.Printf("Me: %s %s [%s]", me.FirstName, me.LastName, me.Username) */
+
 }
