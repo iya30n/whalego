00ac9ec36794f70ddf6d528197443665826b91e8
yasin <yascanary0@gmail.com>
2023-05-30 11:26:25 +0330

    FEAT: run commands manually

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
 	"time"
 	"whalego/connection"
 	"whalego/database/migration"
@@ -16,10 +15,13 @@ import (
 
 func main() {
 
-	if len(os.Args) > 1 {
-		fmt.Println("command migrate is running...")
+	go tickerHandler()
 
-		switch os.Args[1] {
+	for {
+		var cmd string
+		fmt.Scanln(&cmd)
+
+		switch cmd {
 		case "migrate":
 			migration.Migrate()
 		case "test":
@@ -28,13 +30,25 @@ func main() {
 			me, err := con.GetMe()
 			errorHandler.LogFile(err)
 			log.Printf("Me: %s %s [%s]", me.FirstName, me.LastName, me.Username)
+		case "proxy:crawl":
+			fmt.Println("command proxy:crawl is running...")
+			channels := Channel.New()
+			for _, channel := range channels.All() {
+				ProxyService.GetProxies(&channel)
+			}
+		case "proxy:send":
+			fmt.Println("command proxy:send is running...")
+			ProxyService.SendProxy()
+		case "proxy:check":
+			fmt.Println("command proxy:check is running...")
+			ProxyService.CheckAllProxies()
 		default:
 			log.Println("Invalid argument!")
 		}
-
-		return
 	}
+}
 
+func tickerHandler() {
 	crawlerTicker := time.NewTicker(time.Hour * 2)
 
 	sendTicker := time.NewTicker(time.Hour)
@@ -56,16 +70,4 @@ func main() {
 			ProxyService.CheckAllProxies()
 		}
 	}
-
-	/* fmt.Println("command test is running...")
-	chatID, err := ChatService.GetChatId("iya30n")
-	errorHandler.LogFile(err)
-	MessageService.SendMarkdown(chatID.Id, "salam from whale")
-
-	fmt.Println("command getme is running...")
-	con := connection.TdConnection(true)
-	me, err := con.GetMe()
-	errorHandler.LogFile(err)
-	log.Printf("Me: %s %s [%s]", me.FirstName, me.LastName, me.Username) */
-
 }
